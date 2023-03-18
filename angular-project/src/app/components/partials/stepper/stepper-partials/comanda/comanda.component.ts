31c6f71c07226fe9756aab4422aedcc6a8921cf1
AlexWebicon <imac1@192-168-0-100.rdsnet.ro>
2023-03-18 16:19:27 +0200

    Formular comanda step final

--- angular-project/src/app/components/partials/stepper/stepper-partials/comanda/comanda.component.ts
+++ angular-project/src/app/components/partials/stepper/stepper-partials/comanda/comanda.component.ts
@@ -8,23 +8,51 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./comanda.component.scss']
 })
 export class ComandaComponent implements OnInit {
-    public form: FormGroup;
+    public form_persoanafizica: FormGroup;
+    public form_persoanajuridica: FormGroup;
 
   constructor( 
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
-        numar_telefon: this.formBuilder.control('', [Validators.required]),
+    this.form_persoanafizica = this.formBuilder.group({
+      
         nume: this.formBuilder.control('', [Validators.required]),
         prenume: this.formBuilder.control('', [Validators.required]),
-        info_suplimentare: this.formBuilder.control(''),
+        numar_telefon: this.formBuilder.control('', [Validators.required]),
+        adresaemail: this.formBuilder.control('', [Validators.required]),
+        judet: this.formBuilder.control('', [Validators.required]),
+        oras: this.formBuilder.control('', [Validators.required]),
+        judet_livrare: this.formBuilder.control('', [Validators.required]),
+        oras_livrare: this.formBuilder.control('', [Validators.required]),
+        adresa_de_facturare: this.formBuilder.control('', [Validators.required]),
+        adresa_de_livrare: this.formBuilder.control('', [Validators.required]),
       });
 
-      this.form.valueChanges.subscribe((value) => console.log(value));
+      this.form_persoanafizica.valueChanges.subscribe((value) => console.log(value));
+
+
+      this.form_persoanajuridica = this.formBuilder.group({
+      
+        nume: this.formBuilder.control('', [Validators.required]),
+        prenume: this.formBuilder.control('', [Validators.required]),
+        numar_telefon: this.formBuilder.control('', [Validators.required]),
+        adresaemail: this.formBuilder.control('', [Validators.required]),
+        judet: this.formBuilder.control('', [Validators.required]),
+        oras: this.formBuilder.control('', [Validators.required]),
+        judet_livrare: this.formBuilder.control('', [Validators.required]),
+        oras_livrare: this.formBuilder.control('', [Validators.required]),
+        adresa_de_facturare: this.formBuilder.control('', [Validators.required]),
+        adresa_de_livrare: this.formBuilder.control('', [Validators.required]),
+      });
+
+      this.form_persoanajuridica.valueChanges.subscribe((value) => console.log(value));
+
+
   }
 
 submitContactInfo (){
-    console.log(this.form.value);
+    console.log(this.form_persoanafizica.value);
+    console.log(this.form_persoanajuridica.value);
   }
 }
