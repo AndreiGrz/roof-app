53de26660d2e1f394bb2ff89c31e765422f37246
ilie.amariucai@cgm.com <ilie.amariucai@cgm.com>
2023-04-25 00:07:06 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
 const cors = require('cors');
 const chalk = require('chalk');
-const db = require('./app/database/db.js');
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(cors());
+const corsOptions = {
+  origin: ['https://calculator.tabla-online.ro', 'https://tabla-online.ro'],
+  optionsSuccessStatus: 200
+};
+
+app.use(process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? cors(corsOptions) : cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'dist/angular-project')));
+
+  app.get('/calculator-1-apa', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  });
+  app.get('/calculator-2-ape', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  });
+  app.get('/calculator-4-ape', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  });
+  app.get('/calculator-personalizat', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  });
+  app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  });
+}
 
 require('./app/routes/main.routes')(app);
 
