9b72f846385f1c6688338a4b3b487fd3cfae3262
ilie.amariucai@cgm.com <ilie.amariucai@cgm.com>
2023-04-25 09:33:20 +0300

    comment code + create todo

--- server/server.js
+++ server/server.js
@@ -28,9 +28,12 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
   app.get('/calculator-personalizat', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
   });
-  app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
-  });
+  
+  //TODO: create catch-all route with regex
+
+  // app.use((req, res) => {
+  //   res.status(404).sendFile(path.join(__dirname, 'dist/angular-project/index.html'));
+  // });
 }
 
 require('./app/routes/main.routes')(app);
