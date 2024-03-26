4faef0dee783213e3bf30f0d1663854ca3eaad90
agurzun <andrei.gurzun@tss-yonder.com>
2024-03-26 13:30:01 +0200

    change stepper labels

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -12,19 +12,19 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor() {
     this.navLinks = [
       {
-        label: 'Calculator 1 apa',
+        label: 'Acoperis 1 apa',
         link: './calculator-1-apa'
       },
       {
-        label: 'Calculator 2 ape',
+        label: 'Acoperis 2 ape',
         link: './calculator-2-ape'
       },
       {
-        label: 'Calculator 4 ape',
+        label: 'Acoperis 4 ape',
         link: './calculator-4-ape'
       },
       {
-        label: 'Calculator personalizat',
+        label: 'Acoperis personalizat',
         link: './calculator-personalizat'
       }
     ];
